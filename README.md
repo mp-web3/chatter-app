1b91fa631cedadfffeba4e617d90021378be0a74
mp-web3 <mp.web3.t@gmail.com>
2024-04-15 20:37:01 +0200

    set up chatter-next-app repo

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # chatter-app
+
+### Project Setup
+
+1. Init forge directory
+
+To run the script use: 
+forge script ./script/Chatter.s.sol --rpc-url http://localhost:8545 --broadcast --sender 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --unlocked --chain-id 31337
+
+Send 1 ether from an address generated with anvil to my MetaMask address on Anvil Network: 
+cast send --value 1ether --unlocked --from 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 0x9c03Ce240E2D6EEB70B7Ebe73B1289EF4ecBF5A6
+
+2. Create next application (accept default settings)
+    - npx create-next-app@latest
+
+3. Install RainbowKit
+    - npm install @rainbow-me/rainbowkit wagmi viem@2.x @tanstack/react-query
+
+4. Copy paste code in next.config.js from https://github.com/rainbow-me/rainbowkit/blob/main/examples/with-next-app/next.config.js
---
+++ chatter-next-app/app/providers.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import * as React from "react";
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  getDefaultConfig,
+} from "@rainbow-me/rainbowkit";
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import {
+    Chain,
+    sepolia,
+} from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+
+const { wallets } = getDefaultWallets();
+
+const anvil: Chain = {
+  id: 31_337,
+  name: "Anvil Local",
+  nativeCurrency: {
+    name: "tETH",
+    symbol: "tETH",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: { http: ["http://localhost:8545"] },
+    },
+  testnet: true
+};
+
+const config = getDefaultConfig({
+  appName: "RainbowKit demo",
+  projectId: "YOUR_PROJECT_ID",
+  wallets: [
+    ...wallets,
+    {
+      groupName: "Other",
+      wallets: [argentWallet, trustWallet, ledgerWallet],
+    },
+  ],
+    chains: [
+      sepolia,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
+  ],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
